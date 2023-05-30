502eb57e22b7d9ccb28eefc1f4dcd10a5325675f
AK BOTS DEVELOPER <108203396+Aneebrahman@users.noreply.github.com>
2023-05-30 08:56:40 +0530

    Update banned.py

--- plugins/banned.py
+++ plugins/banned.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from utils import temp
 from pyrogram.types import Message
-from database.users_chats_db import db
+from Database.users_chats_db import db
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from info import SUPPORT_CHAT
 
